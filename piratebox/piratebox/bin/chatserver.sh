427b1546aabf2d06661a0b5a4ee74e5ce3f4e19a
Matthias Strubel <matthias.strubel@aod-rpg.de>
2012-02-09 21:06:04 +0100

    created branch with lighttpd scripts only

--- piratebox/piratebox/bin/chatserver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd $1
-#using exec replaces the current running session, which provides the correct pid on the start-stop-daemon
-exec python pirateboxchat_start.py 
-
-
--- piratebox/piratebox/bin/piratebox_droopy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-. $1
-
-/opt/piratebox/bin/droopy -d $FOLDER -p $IMAGE -m \"$TEXT\"
