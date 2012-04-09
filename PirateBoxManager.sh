6913e24690fb44959139d4e930888f3054c756f9
Cale Black <cablack@rams.colostate.edu>
2012-04-09 06:50:26 -0700

    Updated to Version (0.8.0)

--- PirateBoxManager.sh
+++ PirateBoxManager.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-#
-#
-#
-#                                                                                                     
+ 
+#!/bin/bash'\E[47;31m'
+                                                                                                   
 # _|_|_|_|_|                                                  _|_|_|                _|                
 #     _|      _|_|    _|  _|_|  _|  _|_|    _|_|    _|  _|_|  _|    _|  _|    _|  _|_|_|_|    _|_|    
 #     _|    _|_|_|_|  _|_|      _|_|      _|    _|  _|_|      _|_|_|    _|    _|    _|      _|_|_|_|  
@@ -21,31 +19,31 @@
 # Credit for the wonderful scripts goes to Matthias Strubel
 #                       
 ###########################################################################################################################
-###########Version 0.7.7###################################################################################################
-# Updated for Version .4 of PirateBoxScripts
-# Created Configuration Sub-Menu
+###########Version 0.8.0###################################################################################################
+# Added Color to splash screen
+# Added quick change for SSID and Channel Number
+# Added clear upload / download folder in config (may move it to main menu)
+# Updated download only for script version .4
 ###########################################################################################################################
-#Final menu will be: []
+#Final menu will be: [X]
 #1) Install PirateBox (only run one time) [X]
 #2) Start PirateBox [X]
 #3) Stop PirateBox [X]
-#4) Manage Files []
+#4) Manage Files [X]
 #5) Download scripts only [X]
-#6) Configure []
+#6) Configure [X]
 #7) Exit [X]
 ###########################################################################################################################
 #Add manage files (opens PB folder in nautilaus) []
 ###########################################################################################################################
 #Create a Config option and include hostapd.conf and piratebox.conf [X]
 #Add quick configuration options:
-#	- Change SSID []
-#	- Change Channel []
-#	- Change PirateBox upload / download directory []
-#	- Clear PirateBox upload / download directory []
+#	- Change SSID [X]
+#	- Change Channel [X]
+#	- Change PirateBox upload / download directory 
+#	- Clear PirateBox upload / download directory
 ###########################################################################################################################
-#View Upload folder []
-#Possible to do real time updates?
-#Interface for managing files (may wait until GUI)
+#View Upload folder [X]
 ###########################################################################################################################
 #Add Debugging for: []
 #No package for dnsmasq and hostapd (Older versions 10.04 and below do not have universe debs active by default, see comments in option 1) []
@@ -68,20 +66,18 @@
 #PirateBox Manager LiveCD version
 #Disables install option (will be installed by default)
 ###########################################################################################################################
-
-echo "
-__________.__                 __         __________  .____    ____.         
+clear
+echo -e '\E[0;30m'"\033[1m
+__________ __                 __         __________   ____    ____          
 \______   \__|____________  _/  |_   ____\______   \ |   _|  |_   | ___  ___
  |     ___/  |\_  __ \__  \ \   __\_/ __ \|    |  _/ |  |      |  | \  \/  /
  |    |   |  | |  | \// __ \_|  |  \  ___/|    |   \ |  |      |  |  >    < 
  |____|   |__| |__|  (____  /|__|   \___  >______  / |  |_    _|  | /__/\_ |
- 	 	          \/            \/       \/  |____|  |____|       \/
-
-             
-"                                   
-
+ 	 	          \/            \/       \/  |____|  |____|       \/        
+\033[0m"                                   
+echo -e '\E[0;0m'""
 PS3='Please enter your choice: '
-options=("Quick install for Debian based systems" "Start PirateBox" "Stop PirateBox" "Just Download the Scripts" "Configure" "Quit")
+options=("Quick install for Debian based systems" "Start PirateBox" "Stop PirateBox" "Manage PirateBox Download / Upload Folder" "Just Download the Scripts" "Configure" "Quit")
 
 select opt in "${options[@]}"
 do
@@ -134,11 +130,15 @@ echo "5) Configure"
 echo "6) Quit"
 ;;
 
+"Manage PirateBox Download / Upload Folder")
+sudo nautilus /opt/piratebox/tmp &
+;;
+
 "Just Download the Scripts")
-sudo wget -P /tmp/ http://piratebox.aod-rpg.de/dokuwiki/lib/exe/fetch.php?media=piratebox-0.3.3-scripts-1.9.tar.gz
+sudo wget -P /tmp/ http://piratebox.aod-rpg.de/dokuwiki/lib/exe/fetch.php?media=piratebox-scripts_0.4.tar.gz
 cd /tmp/
-sudo cp -i fetch.php?media=piratebox-0.3.3-scripts-1.9.tar.gz piratebox-0.3.3-scripts-1.9.tar.gz
-sudo rm fetch.php?media=piratebox-0.3.3-scripts-1.9.tar.gz
+sudo cp -i fetch.php?media=piratebox-scripts_0.4.tar.gz piratebox-scripts_0.4.tar.gz
+sudo rm fetch.php?media=piratebox-scripts_0.4.tar.gz
 echo "Files saved in /tmp"
 echo ""
 echo "1) Quick install for Debian based systems"
@@ -155,10 +155,39 @@ break
 "Configure")
 echo ""
 PS3='Please enter your choice: '
-config=("Edit piratebox.conf" "Edit hostapd.conf" "Quit")
+config=("Clear PirateBox Upload Folder" "Change SSID" "Change Channel" "Edit piratebox.conf" "Edit hostapd.conf" "Quit")
 select config in "${config[@]}"
 do
     case $config in
+"Clear PirateBox Upload Folder")
+#read -p "Remove all files AND sub-directories (y/n)"
+#[ "$REPLY" == "y" ]
+rm -rf /opt/piratebox/share/*
+echo "Deleted all files in the directory"
+#[ "$REPLY" != "y" ]
+#rm /opt/piratebox/share/*
+;;
+"Change SSID")
+echo "Enter new SSID: "
+read SSID
+sed '3s/.*/ssid='$SSID'/' </opt/piratebox/conf/hostapd.conf> /opt/piratebox/conf/hostapd.conf.new
+cp -i /opt/piratebox/conf/hostapd.conf /opt/piratebox/hostapd.conf.bak
+cp -i /opt/piratebox/conf/hostapd.conf.new /opt/piratebox/hostapd.conf
+;;
+
+"Change Channel")
+echo "Enter Channel [1-14]:"
+read CHANNEL
+#if [ $CHANNEL > 14 ];
+#then
+#echo "Must be less than 14"
+#fi
+sed '5s/.*/channel='$CHANNEL'/' </opt/piratebox/conf/hostapd.conf> /opt/piratebox/conf/hostapd.conf.new
+cp -i /opt/piratebox/conf/hostapd.conf /opt/piratebox/hostapd.conf.bak
+cp -i /opt/piratebox/conf/hostapd.conf.new /opt/piratebox/hostapd.conf
+echo "The last revision is backed up in /opt/piratebox/conf/hostapd.conf.bak"
+;;
+
 "Edit piratebox.conf")
 nano /opt/piratebox/conf/piratebox.conf
 ;;
@@ -166,6 +195,7 @@ nano /opt/piratebox/conf/piratebox.conf
 "Edit hostapd.conf")
 nano /opt/piratebox/conf/hostapd.conf
 ;;
+ 
 "Quit")
 break
 ;;
